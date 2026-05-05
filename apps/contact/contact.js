c12e260148484a7529d2bd14deac4195c72f8d14
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-05-05 05:50:40 +0530

    secure contact.html link

--- apps/contact/contact.js
+++ apps/contact/contact.js
@@ -1,7 +1,17 @@
-let user_email = sessionStorage.getItem("user")
-if (localStorage.getItem(user_email + "image") != null) {
-    let profile_pic = document.getElementById("profile_pic")
-    profile_pic.style.display = "none"
+// add new contact box 
+let add_icon = document.getElementById("new_contact")
+add_icon.onclick = function () {
+    let bg = document.getElementById("contact_bg")
+    bg.style.display = "block"
+
 }
 
+let close = document.getElementById("close")
+close.onclick = function () {
+    let bg = document.getElementById("contact_bg")
+    bg.style.display = "none"
+}
 
+if (sessionStorage.getItem("user") == null) {
+    window.location.replace("../../index.html")
+}
